c6ac2946d76fda9e319c15a322bbb0443dde128f
Domingos Santiago <dmgssantiago@gmail.com>
2025-02-22 21:46:52 -0300

    Criação do componente Clubes

---
+++ src/components/Clubes.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header.jsx"
+export function Clubes() {
+    return (
+        <div>
+            <Header />
+            <div>
+            </div>
+            
+            <div>
+            
+            </div>
+        </div>
+    )
+}
