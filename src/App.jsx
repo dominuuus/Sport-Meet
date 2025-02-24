8527cf6fe1ed93bff4a2fa4148e2c63cdad921c0
Domingos Santiago <dmgssantiago@gmail.com>
2025-02-24 16:15:06 -0300

    Interação posts e comentários
    Criação das interações entre posts e comentários

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,36 @@ import { Post } from './components/Posts'
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/dominuuus.png',
+      name: 'Domingos Santiago',
+      role: '@dominuuus',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera!'},
+      { type: 'paragraph', content: 'Hoje a pedalada foi maravilhosa. Vem fazer parte do grupo'},
+      { type: 'paragraph', content: 'Todo sábado às 9h da manhã'},
+    ],
+    publishedAt: new Date('2025-01-01 18:44:33'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/LinuxJS.png',
+      name: 'Thyana Lira',
+      role: '@thay_lira',
+    },
+    content: [
+      { type: 'paragraph', content: 'Arrasou amigo'},
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-01-01 18:44:33'),
+  },
+];
 
 function App() {
   return (
@@ -13,7 +43,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map(post => {
+            return (
+            <Post 
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+          })}
         </main>
       </div>
     </>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,12 @@ import icons from "../icons/icon.js";
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar.jsx";
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+    
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src={icons.profileAvatar}/>
@@ -14,14 +19,14 @@ export function Comment() {
                     <header>
                         <div className={styles.authorAndTime}>
                             <strong>Thayana Lira</strong>
-                            <time title="22 de fevereiro às 10:20" datetime="2025-02-22 10:20:33">Cerca de 1h atrás</time>
+                            <time title="22 de fevereiro às 10:20" dateTime="2025-02-22 10:20:33">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar o comentário">
+                        <button onClick={handleDeleteComment} title="Deletar o comentário">
                             <Trash size={24}/>
                         </button>
                     </header>
-                    <p>Parabéns pelo conteúdo</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
